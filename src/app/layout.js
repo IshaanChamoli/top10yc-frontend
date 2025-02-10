07eaa953bd9f4291176efcdc9a8b781446a324c2
Ishaan Chamoli <ishaan.c.edu@gmail.com>
2025-02-10 16:44:56 -0600

    analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,8 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { FirebaseInitializer } from './components/FirebaseInitializer';
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,6 +17,8 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <FirebaseInitializer />
         {children}
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
